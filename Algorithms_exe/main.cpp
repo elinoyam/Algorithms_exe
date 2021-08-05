6df1d9dafe802cb0aa7ab0ebd2638f97a898176b
Elinoy Amar <amarelinoy@gmail.com>
2021-08-05 15:54:38 +0300

    working gooddddd!!!!

--- Algorithms_exe/main.cpp
+++ Algorithms_exe/main.cpp
@@ -15,23 +15,23 @@ int main2() {
     int result = -1;    
 
     cin >> numberString;
-    if (!checkIfNumber(numberString) && !WrongInput) {
-        cout << "wrong input" << endl;
+    if (!checkIfNumber(numberString) || cin.eof()){
+        cout << "invalid input" << endl;
         WrongInput = true;        
     }
     numberOfVertexes = atoi(numberString.c_str());
     Graph mainGraph(numberOfVertexes);
 
     cin >> numberString;
-    if (!checkIfNumber(numberString) && !WrongInput) {
-        cout << "wrong input" << endl;
+    if ((!checkIfNumber(numberString) || cin.eof()) && !WrongInput) {
+        cout << "invalid input" << endl;
         WrongInput = true;  
     }
     sourceVertex = atoi(numberString.c_str());
 
     cin >> numberString;
-    if (!checkIfNumber(numberString) && !WrongInput) {
-        cout << "wrong input" << endl;
+    if ((!checkIfNumber(numberString) || cin.eof()) && !WrongInput) {
+        cout << "invalid input" << endl;
         WrongInput = true;  
     }
     targetVertex = atoi(numberString.c_str());
@@ -45,8 +45,11 @@ int main2() {
             result = 0;
         }
         catch (exception ex) {
-            cout << "wrong input" << endl;
+            cout << "invalid input" << endl;
         }
+    }
+    else {
+
     }
 
     return result;
